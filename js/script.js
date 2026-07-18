220ec6e6ff4ec822115f0697f15c3858ecd60b28
unemichi14-rgb <unemichi14@gmail.com>
2026-07-18 17:18:24 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+//スムーススクロール（jQuery）SP+PC
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "linear", //swing or linear
+  );
+});
+
+//ドロワーメニューを閉じる
+
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+
